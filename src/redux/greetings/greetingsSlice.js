95c9ea10b1d8435d17f8af488e8ae9701e95b472
tumainimaganiko <tumainimaganiko@gmail.com>
2023-09-21 10:31:57 +0300

    Create Greetings Slice

---
+++ src/redux/greetings/greetingsSlice.js
@@ -0,0 +1,29 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  greetings: [],
+};
+
+export const fetchGreetings = createAsyncThunk(
+  'rockets/fetchRockets',
+  async () => {
+    const response = await axios('http://localhost:3000/greetings/random');
+    return response.data;
+  },
+);
+
+const greetingsSlice = createSlice({
+    name: 'greetings',
+    initialState,
+    reducers: {
+
+    },
+    extraReducers: (builders) => {
+      builders.addCase(fetchGreetings.fulfilled, (state, action) => {
+        state.greetings = action.payload;
+      });
+    },
+})
+
+export default greetingsSlice.reducer;
